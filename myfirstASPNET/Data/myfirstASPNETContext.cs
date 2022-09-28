33f195a95128791cc162061386a5e43642dcdfc5
Aaliya313 <aaliyaladha@hotmail.co.uk>
2022-09-28 12:20:21 +0100

    Add project files.

---
+++ myfirstASPNET/Data/myfirstASPNETContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using myfirstASPNET.Models;
+
+namespace myfirstASPNET.Data.StudentDb
+{
+    public class myfirstASPNETContext : DbContext
+    {
+        public myfirstASPNETContext (DbContextOptions<myfirstASPNETContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<myfirstASPNET.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ myfirstASPNET/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myfirstASPNET.Models
+{
+    public class Student
+    {
+        //guid is a datatype for ID, builds more of a robust system
+        public Guid Id { get; set; } //must have an Id, must have a 'primary key' like in SQL
+        [Required] //ensures it gets filled out
+        [Display(Name = "First Name")] //label to assign to details of these
+        //Loads of other [] rules you can use - explore them
+        public string FirstName { get;set; }
+        [Required] //square brackets to what's the next line under it
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Student Email")]
+        [DataType(DataType.EmailAddress)] //ensures email address is correct
+        public string email { get; set; }
+        public int CourseID { get; set; }
+        
+    }
+}
